6039d83b2ccd5e1e04b66d6d2cdf8aa1af908799
MaksymKokocinski <71514845+MaksymKokocinski@users.noreply.github.com>
2020-11-11 17:51:33 +0100

    Add files via upload

--- AutoPass/database.py
+++ AutoPass/database.py
@@ -1,51 +1,56 @@
 import sqlite3
 import bcrypt
-#ogarnac ten syf
+
 class Database:
     def __init__(self):
+        #lokalizacja bazy danych i cursor do bazy danych
         try:
-            self.conn = sqlite3.connect("test.db")
+            self.conn = sqlite3.connect("ProPython/AutomationProjects/AutoPass/AutoPassDatabase.db")
             print("Successfully Opened Database")
             self.curr = self.conn.cursor()
         except:
             print("Failed")
     
     def createTable(self):
+        #tworzenie tabeli z loginami w bazie danych
+        create_table = """
+        CREATE TABLE IF NOT EXISTS cred(
+        id Integer PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL);
         """
-        Method for Creating Table in Database
-        """
-        create_table = """CREATE TABLE IF NOT EXISTS cred(
-            id Integer PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL,
-            password TEXT NOT NULL
-            );
-            """
         self.curr.execute(create_table)
         self.conn.commit()
 
     def insertData(self,data):
-        """Method for inserting Data in Table in Database"""
+        #wprowadzanie danych do bazy danych
 
-        insert_data= """INSERT INTO cred(username, password)
-        VALUES(?, ?);
+        insert_data= """
+        INSERT INTO cred(username, password) VALUES(?, ?);
         """
         self.curr.execute(insert_data, data)
         self.conn.commit()
+
     def searchData(self, data):
-        """Method for Searching Data in Table in Database"""
-        search_data ="""SELECT * FROM cred WHERE username = (?);"""
+        #sprawdzanie czy taki login jest juz w bazie danych
+        search_data ="""
+        SELECT * FROM cred WHERE username = (?);
+        """
         self.curr.execute(search_data, data)
+
         rows = self.curr.fetchall()
 
         if rows == []:
             return 1
-        return 0
+        else:
+            return 0
     def validateData(self, data, inputData):
-        """Method for Validating Data Table in Database"""
-        print("data:",data)
-        print("inputdata:",inputData)
-        validate_data = """SELECT * FROM cred WHERE username = (?);"""
+        #uwierzytelnianie danych przed zalogowaniem do main window
+
+        validate_data = """
+        SELECT * FROM cred WHERE username = (?);
+        """
         self.curr.execute(validate_data, data)
         row = self.curr.fetchall()
+
         if row[0][1] == inputData[0]:
             return row[0][2] == bcrypt.hashpw(inputData[1].encode(), row[0][2])
+        
--- AutoPass/generator.py
+++ AutoPass/generator.py
@@ -1,36 +1,35 @@
 import random
 #import array
 
-#zastanowic sie czy nie zlosowac hasla jeszce raz albo jakis element losowosci w nim wprowadzic
 # zrobic ze imput musi byc do kazdego hasla z tytulem do czego ono jest
 class RandomPass():
     def __init__(self):
-        maxlen = 12
+        self.maxlen = 12
 
-        digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']  
+        self.digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']  
 
-        smallchar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o',
+        self.smallchar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o',
                     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z'] 
         
-        bigchar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O',
+        self.bigchar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O',
                 'p', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z'] 
         
-        symb = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+        self.symb = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
                 '*', '(', ')', '<','&','#'] 
         #laczenie wszystkich znakow
-        allchar= digits + smallchar + bigchar + symb
+        self.allchar= self.digits + self.smallchar + self.bigchar + self.symb
 
         #losowy znak bd z kazdej z tabeli
-        randomdigit = random.choice(digits)
+        #nie jest chyba potrzebny bo mozna wygenerowac nastepne
+        """randomdigit = random.choice(digits)
         randomsmallchar = random.choice(smallchar)
         randombigchar = random.choice(bigchar)
-        randomsymbol = random.choice(symb)
-
+        randomsymbol = random.choice(symb)"""
+        
         #robienie sie hasla
-        temppass = ""
-        for letter in range(maxlen-4):
-            temppass = temppass + random.choice(allchar)
-        temppass = temppass + randomdigit + randomsmallchar + randombigchar + randomsymbol
-        print(temppass)
+        self.temppass = ""
+        for letter in range(self.maxlen):
+            self.temppass = self.temppass + random.choice(self.allchar)
+        print(self.temppass)
 
 RandomPass()
