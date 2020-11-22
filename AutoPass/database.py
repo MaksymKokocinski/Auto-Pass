cf23b49e4de1ee5c7d1b9021ade85f1d51bad634
MaksymKokocinski <71514845+MaksymKokocinski@users.noreply.github.com>
2020-11-22 10:36:44 +0100

    Add files via upload

--- AutoPass/database.py
+++ AutoPass/database.py
@@ -47,17 +47,17 @@ class Database:
         validate_data = """
         SELECT * FROM accounts WHERE username = (?);
         """
+        print("data:",data)
+        print("input data:",inputData)
         self.curr.execute(validate_data, data)
         row = self.curr.fetchall()
+        print("row",row)
         self.userMaker()
-        
+        #print("cos tam",row[0][1])
+        #print("cos tam 2:",inputData[0])
         if row[0][1] == inputData[0]:
-            return row[0][2] == bcrypt.hashpw(inputData[1].encode(), row[0][2])
-
-        
-
-    """Wszystko od tego momentu jest do tabeli wyświetlanych w głownym menu """
-           
+            return row[0][2] == bcrypt.hashpw(inputData[1].encode(), row[0][2])#tutaj encoduje
+                       
     def userMaker(self):
         self.curr.execute("SELECT count(id) FROM accounts")
         datanumber = self.curr.fetchall()
@@ -67,19 +67,71 @@ class Database:
             usernumber = 0
         else:
             usernumber = 1    
+##################################################################################################
+
+class Database2:
+    def __init__(self):
+        #lokalizacja bazy danych i kursor do bazy danych
+        try:
+            self.conn = sqlite3.connect("ProPython/AutomationProjects/AutoPass/AutoPassDatabase.db")
+            #print("Successfully Opened Database")
+            self.curr = self.conn.cursor()
+        except:
+            print("Failed")
+    
+    def createTable(self):
+        #tworzenie tabeli z loginami w bazie danych
+        create_table = """
+        CREATE TABLE IF NOT EXISTS userdata(
+        id Integer PRIMARY KEY AUTOINCREMENT, platform TEXT NOT NULL, password TEXT NOT NULL);
+        """
+        self.curr.execute(create_table)
+        self.conn.commit()
+
+    def insertData(self,data):
+        #wprowadzanie danych do bazy danych
+        insert_data= """
+        INSERT INTO userdata(platform, password) VALUES(?, ?);
+        """
+        self.curr.execute(insert_data, data)
+        self.conn.commit()
+        #self.userMaker()
+
+    def searchData(self, data):
+        #sprawdzanie czy taki login jest juz w bazie danych
+        search_data ="""
+        SELECT * FROM userdata WHERE platform = (?);
+        """
+        self.curr.execute(search_data, data)
+        rows = self.curr.fetchall()
+        if rows == []:
+            return 1
+        else:
+            return 0
+    def validateData(self, data, inputData):
+        #uwierzytelnianie danych przed zalogowaniem do main window
+
+        validate_data = """
+        SELECT * FROM userdata WHERE platform = (?);
+        """
+        self.curr.execute(validate_data, data)
+        row = self.curr.fetchall()
         
+        print("cos tam",row[0][1])
+        print("cos tam 2:",inputData[0])
+        if row[0][1] == inputData[0]:
+            return row[0][2] == bcrypt.hashpw(inputData[1].encode(), row[0][2])
+                       
+    def userMaker(self):
+        self.curr.execute("SELECT count(id) FROM userdata")
+        datanumber = self.curr.fetchall()
+        #print("datanumber:",datanumber[0])
+        global usernumber
+        if str(datanumber[0]) =="(0,)":
+            usernumber = 0
+        else:
+            usernumber = 1
         
-        """for temp1 in range(100):
-            temp1 = temp1 + 1
-            temp2 = str(temp1)
-            temp2 = '('+temp2 + ','+')'
-            print(temp2)
-            if temp2 == str(datanumber[0]):
-                print("mamy to",temp1)
-                global usernumber
-                usernumber =str(temp1)"""
-        #self.createTable2()
-                    
 
 
         
